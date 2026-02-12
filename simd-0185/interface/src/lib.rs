5b5ffd0bb18875d6cffc9720d71fe7a5a47e965d
Joe Caulfield <jcaulfield135@gmail.com>
2026-02-12 11:49:15 +0000

    simd-0185: expand on interface

--- simd-0185/interface/src/lib.rs
+++ simd-0185/interface/src/lib.rs
@@ -4,7 +4,6 @@ use sha2_const_stable::Sha256;
 use solana_instruction::{AccountMeta, Instruction};
 use solana_pubkey::Pubkey;
 
-#[repr(u8)]
 pub enum ProgramInstruction {
     /// Create a v4 vote account via CPI.
     ///
@@ -14,7 +13,11 @@ pub enum ProgramInstruction {
     /// 1. `[ ]` Identity PDA (below)
     /// 2. `[ ]` Rent sysvar
     /// 3. `[ ]` Clock sysvar
-    Create,
+    Create {
+        authorized_voter: Pubkey,
+        authorized_withdrawer: Pubkey,
+        commission: u8,
+    },
 
     /// Read the contents of a v4 vote account.
     ///
@@ -25,15 +28,40 @@ pub enum ProgramInstruction {
 }
 
 impl ProgramInstruction {
+    const CREATE: u8 = 0;
+    const VIEW: u8 = 1;
+
     pub fn decode(input: &[u8]) -> Self {
         match input.first() {
-            Some(0) => Self::Create,
-            Some(1) => Self::View,
+            Some(&Self::CREATE) => {
+                let authorized_voter =
+                    Pubkey::new_from_array(input[1..33].try_into().unwrap());
+                let authorized_withdrawer =
+                    Pubkey::new_from_array(input[33..65].try_into().unwrap());
+                let commission = input[65];
+                Self::Create {
+                    authorized_voter,
+                    authorized_withdrawer,
+                    commission,
+                }
+            }
+            Some(&Self::VIEW) => Self::View,
             _ => panic!("invalid instruction"),
         }
     }
 
-    pub fn create(vote_account: &Pubkey) -> Instruction {
+    pub fn create(
+        vote_account: &Pubkey,
+        authorized_voter: &Pubkey,
+        authorized_withdrawer: &Pubkey,
+        commission: u8,
+    ) -> Instruction {
+        let mut data = Vec::with_capacity(66);
+        data.push(Self::CREATE);
+        data.extend_from_slice(authorized_voter.as_ref());
+        data.extend_from_slice(authorized_withdrawer.as_ref());
+        data.push(commission);
+
         Instruction {
             program_id: PROGRAM_ID_AS_PUBKEY,
             accounts: vec![
@@ -42,7 +70,7 @@ impl ProgramInstruction {
                 AccountMeta::new_readonly(solana_sdk_ids::sysvar::rent::ID, false),
                 AccountMeta::new_readonly(solana_sdk_ids::sysvar::clock::ID, false),
             ],
-            data: vec![ProgramInstruction::Create as u8],
+            data,
         }
     }
 
@@ -50,7 +78,7 @@ impl ProgramInstruction {
         Instruction {
             program_id: PROGRAM_ID_AS_PUBKEY,
             accounts: vec![AccountMeta::new_readonly(*vote_account, false)],
-            data: vec![ProgramInstruction::View as u8],
+            data: vec![Self::VIEW],
         }
     }
 }
@@ -77,6 +105,34 @@ pub const fn get_identity_pda() -> Pubkey {
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    #[test]
+    fn test_create_roundtrip() {
+        let vote_account = Pubkey::new_unique();
+        let authorized_voter = Pubkey::new_unique();
+        let authorized_withdrawer = Pubkey::new_unique();
+        let commission = 7;
+
+        let ix = ProgramInstruction::create(
+            &vote_account,
+            &authorized_voter,
+            &authorized_withdrawer,
+            commission,
+        );
+
+        match ProgramInstruction::decode(&ix.data) {
+            ProgramInstruction::Create {
+                authorized_voter: decoded_voter,
+                authorized_withdrawer: decoded_withdrawer,
+                commission: decoded_commission,
+            } => {
+                assert_eq!(decoded_voter, authorized_voter);
+                assert_eq!(decoded_withdrawer, authorized_withdrawer);
+                assert_eq!(decoded_commission, commission);
+            }
+            _ => panic!("expected Create"),
+        }
+    }
 
     #[test]
     fn test_pda() {
